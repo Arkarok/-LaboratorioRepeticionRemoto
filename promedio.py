a5dbb29499d875b57e696c8239666089c7c1f2bb
Nunez Suarez <000400886@bga.upb>
2019-03-01 11:57:04 -0500

    Creación tercer algortimo

---
+++ promedio.py
@@ -0,0 +1,10 @@
+print("Promedio Numeros")
+
+num1=float(input("Digita una nota, Por favor: "))
+num2=float(input("Digita una nota, Por favor: "))
+num3=float(input("Digita una nota, Por favor: "))
+
+sum=num1+num2+num3
+prom=sum/3
+
+print("El promedio es " + str(prom))
