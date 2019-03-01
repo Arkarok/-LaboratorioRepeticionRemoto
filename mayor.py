5553f83ef83da37ad3875b205cbd9c49e1d7a99f
Nunez Suarez <000400886@bga.upb>
2019-03-01 11:28:09 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,8 +1,22 @@
 print("Numero entero mayor que 0")
 
+
 a=int(input("Digita un numero entero, Por favor: "))
+
 
 if a>0:
   print("El numero es mayor que 0")
+
 else:
   print("El numero no es mayor que 0")
+
+print("Numero entero mayor que 0, Modificaciones")
+
+for a in range(-5,5):
+  if a>0:
+    print(str(a) + " El numero es mayor que 0")
+  elif a==0:
+    print(str(a) + " El numero es igual a 0")
+  else:
+    print(str(a) + " El numero no es mayor que 0")
+
