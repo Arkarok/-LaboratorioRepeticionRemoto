70de3e9d039e1c03cdfc354fd53ffce85a7c1546
Nunez Suarez <000400886@bga.upb>
2019-03-01 11:35:26 -0500

    Creación segundo algortimo

---
+++ pares.py
@@ -0,0 +1,9 @@
+print("Numeros Pares")
+
+num=int(input("Digita un numero,Por favor: "))
+
+if num%2==0:
+  print(str(num) + " Es un numero par")
+else:
+  print(str(num) + " No es un numero par")
+  
